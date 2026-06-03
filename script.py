a38efdc04af62e3bc877575681ecfca01572ab6d
Felipe Finelli <felipen08@hotmail.com>
2026-06-03 19:58:55 +0100

    'update' function created, 'add' function refined.

--- script.py
+++ script.py
@@ -1,127 +1,159 @@
+import json
 import os
 import sys
-import json
-from pathlib import Path
 from datetime import datetime
+from pathlib import Path
 
+json_file = Path("database.json")
 
-
-
-json_file = Path("database.json") #definindo o caminho pro python checar
-
-
-
-hora_atual = datetime.now()  #pega valor atual
-
-
-dados_novos = {      # dados a ser lançados, note que o id está vazio e ainda nao é uma lista.
-        "id": "",            
-        "description": "".join(sys.argv[3:]),
-        "status": "todo",
-        "createdAt":hora_atual.strftime("%d/%m/%Y %H:%M"),
-        "updatedAt": ""
-        }
-
-
-
-
+json_exist = json_file.exists()
 
 def add():
 
-        #se o arquivo json nao existir.
-        if not json_file.exists():  
-                
-                
-                #abre no modo "w" de write, para escrever/criar o arquivo json.
-                with open(file = json_file, mode = "w", encoding = "utf-8") as f:
+    criar_tarefa = {
+    "id": "",
+    "description": "".join(sys.argv[3:]),
+    "status": "todo",
+    "createdAt": datetime.now().strftime("%d/%m/%Y %H:%M"),
+    "updatedAt": "",
+}
 
-                        #muda o id para 1 ja que sera a primeira tarefa.
-                        dados_novos["id"] = 1      
-                        
-                        # cria a lista que vai abrigar os dicionarios
-                        dados_novos_criar = [dados_novos]   
+    if not json_exist:
+        with open(file=json_file, mode="w", encoding="utf-8") as f:
+            criar_tarefa["id"] = 1
 
-                        #deposita a primeira task.
-                        json.dump(dados_novos_criar, f, indent = 5, ensure_ascii = False)
+            dados_novos_criar = [criar_tarefa]
+
+            json.dump(dados_novos_criar, f, indent=5, ensure_ascii=False)
+
+    else:
+        with open(file=json_file, mode="r", encoding="utf-8") as f:
+            dados_json = json.load(f)
+
+        todos_ids = []
         
+        for task in dados_json:
+            todos_ids.append(task["id"])
         
-        
-        
-        
-        
+        ultimo_id = max(todos_ids) + 1
+            
+        criar_tarefa["id"] = ultimo_id
 
-        #se o arquivo json já existir. Nesse caso vamos precisar "refazer" o json todo.
-        else:
-                
-                #abrimos no modo "r" de read, para pegarmos os dados do json atual para carregar no python.
-                #nao da pra escrevermos direto no arquivo sem estragar a estrutura de 
-                #dicionario e lista do nosso arquivo json.
-                with open(file = json_file, mode = "r", encoding = "utf-8") as f:
-                        
-                        #criando variavel que vai representar os dados carregados no python.
-                        json_carregado = json.load(f)
-                        
-                        #vendo a quantidade de tasks existentes para saber qual vai ser o id da task atual.
-                        ultimo_id = len(json_carregado) + 1
+        dados_json.append(criar_tarefa)
 
-                        #inserindo o id correto da task que sera lançada
-                        dados_novos["id"] = ultimo_id
-
-                        #adicionando nossa nova task nos dados carregados do json.
-                        json_carregado.append(dados_novos)
-
-                        #por fim vamos utilizar o "w" de write para sobreescrever o arquivo json atual
-                        #com nossa nova task ja inclusa.
-                        with open(file = json_file, mode = "w", encoding = "utf-8") as f:
-                                json.dump(json_carregado, f, indent = 5, ensure_ascii = False)
-
-
-
-
-
-
+        with open(file=json_file, mode="w", encoding="utf-8") as f:
+            json.dump(dados_json, f, indent=5, ensure_ascii=False)
 
 
 def update():
-    print("dummy")
+    if not json_exist:
+         print("Erro! o arquivo database_json não existe.")
+
+    else:
+        ref_id = int(sys.argv[3])
+
+        new_description = "".join(sys.argv[4:])
+
+        with open(file=json_file, mode="r", encoding="utf-8") as f:
+            dados_json = json.load(f)
+        
+        
+        
+        
+        #"elemento" representa cada dicionario dentro da lista dados_json.
+        #vai checar o "id" de cada um e se o id for = ao ref_id, vai atualizar alguns dados
+        # e depois o break vai fazer com que pare a iteracao e ao mesmo tempo evite com que o
+        #bloco else rode.   For + Else é algo compativel.
+
+        #
+        
+        for elemento in dados_json:   
+            if elemento["id"] == ref_id:
+                elemento["description"] = new_description
+                elemento["updatedAt"] = datetime.now().strftime("%d/%m/%Y %H:%M")
+                break
+        else:
+            print("Erro! O id digitado não existe.")
+
+        with open(file = json_file, mode = "w", encoding = "utf-8") as f:
+            json.dump(dados_json, f, indent = 5, ensure_ascii = False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def delete():
     print("dummy")
+
+
 def loop():
     print("dummy")
+
+
 def list():
     print("dummy")
+
+
 def mark_in_progress():
     print("dummy")
+
+
 def mark_done():
     print("dummy")
 
 
-
-
-
-
-
 def main():
-    
     if sys.argv[2] == "add":
-        add()
-   
+        if len(sys.argv) < 4:
+            print("Erro, faltou incluir a descrição.")
+        else:
+            add()
+
     elif sys.argv[2] == "update":
         update()
-    
+
     elif sys.argv[2] == "delete":
         delete()
-   
+
     elif sys.argv[2] == "mark-in-progress":
         mark_in_progress()
-    
+
     elif sys.argv[2] == "mark-done":
         mark_done()
-   
+
     elif sys.argv[2] == "list":
         list()
-    
+
     else:
         loop()
+
 
 main()
